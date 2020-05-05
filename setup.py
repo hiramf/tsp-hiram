54997efb6edd5d83cf93351677e8fa569d75d2a0
Hiram Foster <hiramfoster.co@gmail.com>
2020-05-04 23:20:41 -0400

    numpy upgrade

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     install_requires=[
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
         'mip>=1.8.1',
-        'numpy>=1.18.3',
+        'numpy>=1.18.4',
     ],
     extras_require={
         # eg:
