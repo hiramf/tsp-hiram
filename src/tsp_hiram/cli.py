35dfecac338e1b00dacccdc84a296f3f9b3ca719
Hiram Foster <hiramfoster.co@gmail.com>
2020-05-05 22:24:22 -0400

    add closed arg and default to open

--- src/tsp_hiram/cli.py
+++ src/tsp_hiram/cli.py
@@ -6,6 +6,7 @@ from tsp_hiram import tsp
 parser = argparse.ArgumentParser(description='Solve Traveling Salesman Problem given a list of coordinates')
 parser.add_argument('filename', metavar='FILENAME', type=str, help="File containing the coordinates.")
 parser.add_argument('--max', type=int, default=None, help='Max distance of the route')
+parser.add_argument('--closed', type=bool, default=False, help='Whether a constrained solutions should be an open or closed loop')
 
 
 def main(args=None):
@@ -14,7 +15,7 @@ def main(args=None):
         reader = csv.reader(csvfile, delimiter=',')
         # TODO: More robust handling of csvreader. Currently just drops first row as header and makes a lot of assumptions
         coordinates = [(int(x), int(y)) for [x, y] in [row for row in reader][1:]]
-        route, distance = tsp.optimize(coordinates, max_distance=args.max)
+        route, distance = tsp.optimize(coordinates, max_distance=args.max, closed=args.closed)
 
         if args.max is not None:
             print(f'{len(route)} nodes could be touched with max distance of {args.max}')
